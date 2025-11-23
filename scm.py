e8e3e8f270f6a586b7c4ac688d9639b8688d76fe
c <ckdeutschbein@willamette.edu>
2025-11-23 14:07:54 -0800

    doing an scm or whatever

--- scm.py
+++ scm.py
@@ -3,14 +3,15 @@ import os, sys, json, subprocess, difflib
 # diff helper
 diff_lines = lambda ls_0, ls_1: "".join([line for line in difflib.unified_diff(ls_0, ls_1)])
 
-# Get all file names that aren't hidden
-fs = [os.path.join(node[0],n) for node in os.walk(os.getcwd()) for n in node[2] if "." not in node[2] if "." not in node[0]] 
+# Get all files that aren't hidden
+tree = [node for node in os.walk(os.getcwd()) if "." not in node[0]]
+fs = [os.path.join(node[0],n) for node in tree for n in node[2] if n[0] != "."]   
 
 # Get or create .scm file
-if not os.path.isfile(".scm"):
+if not os.path.isfile(".scm") or os.path.getsize(".scm") == 0:
     # create
-    latest = {f:open(f).read_lines() for f in fs}
-    json.dump({"latest": latest, "commit":[{"init":latest, "diff":{}}], open(".scm", "w"))
+    latest = {f:open(f).read().splitlines() for f in fs}
+    json.dump({"latest": latest, "commit":[{"init":latest, "diff":{}}]}, open(".scm", "w"))
 else:
     # get
     scm = json.loads(open(".scm","r").read())
@@ -20,7 +21,9 @@ else:
     new_fs = [f for f in fs if fs not in old_fs]
     init = {f:open(f).read() for f in new_fs}
     # We use unified diff from difflib since it still works with patch.
-    diff = {f:diff(late[f],open(f).read_lines()) for f in old_fs}
-    scm["latest"] = {f:open(f).read_lines() for f in fs}
+    diff = {f:diff_lines(late[f],open(f).read().splitlines()) for f in old_fs}
+    scm["latest"] = {f:open(f).read().splitlines() for f in fs}
     scm["commit"].append({"init":init,"diff":diff})
-    json.dumps(scm, open(".scm","w"))
+    json.dump(scm, open(".scm","w"))
+
+# Trivial Comment
