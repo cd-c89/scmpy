bbe85c0717bfa33174f208157b8b8fdb39e31732
c <ckdeutschbein@willamette.edu>
2025-11-22 20:58:51 -0800

    initializing scm, kinda

--- scm.py
+++ scm.py
@@ -1,12 +1,14 @@
 import os, sys, json, subprocess
 
 # Get all file names
-fs = [os.path.join(node[0],n) for node in os.walk(os.getcwd()) for n in node[2]] 
+fs = [os.path.join(node[0],n) for node in os.walk(os.getcwd()) for n in node[2] if "." not in node[2] if "." not in node[0]] 
 
 # Get the current .scm file
 if not os.path.isfile(".scm"):
     # init
-    json.dumps(open(".scm","w"), [{f:subprocess.getoutput("diff " + f + "<(printf)") for f in fs}])
+    os.system("touch .temporary")
+    json.dump([{f:subprocess.getoutput("diff .temporary " + f) for f in fs}], open(".scm", "w"))
+    os.system("rm .temporary")
 else:
     # load controls
     curr = json.loads(open(".scm","r").read())
