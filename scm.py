b6e9338c1e6d2c4ccbafdbb08ec6c83ffad2f81d
c <ckdeutschbein@willamette.edu>
2025-11-29 12:38:46 -0800

    make sure you end files with newlines to use patch

--- scm.py
+++ scm.py
@@ -2,8 +2,11 @@
 
 import os, sys, json, subprocess, difflib
 
+SCM_NAME = ".scm"
+DIFF_NAME = ".diff"
+
 # diff helper
-diff_lines = lambda ls_0, ls_1: list(difflib.unified_diff(ls_0, ls_1))
+diff_lines = lambda ls_0, ls_1: [line.rstrip() for line in list(difflib.unified_diff(ls_0, ls_1))]
 
 # saves current version to .scm
 def commit():
@@ -12,16 +15,16 @@ def commit():
     fs = [os.path.join(node[0], n) for node in tree for n in node[2] if n[0] != "."]
 
     # Get or create .scm file
-    if not os.path.isfile(".scm") or os.path.getsize(".scm") == 0:
+    if not os.path.isfile(SCM_NAME) or os.path.getsize(".scm") == 0:
         # create
         latest = {f: open(f).read().splitlines() for f in fs}
         json.dump(
             {"latest": latest, "commit": [{"init": latest, "diff": {}}]},
-            open(".scm", "w"),
+            open(SCM_NAME, "w"),
         )
     else:
         # get
-        scm = json.loads(open(".scm", "r").read())
+        scm = json.loads(open(SCM_NAME, "r").read())
         late = scm["latest"]
         curr = scm["commit"]
         old_fs = [f for f in curr[-1]["init"]] + [f for f in curr[-1]["diff"]]
@@ -31,33 +34,35 @@ def commit():
         diff = {f: diff_lines(late[f], open(f).read().splitlines()) for f in old_fs}
         scm["latest"] = {f: open(f).read().splitlines() for f in fs}
         scm["commit"].append({"init": init, "diff": diff})
-        json.dump(scm, open(".scm", "w"))
+        json.dump(scm, open(SCM_NAME, "w"))
 
 
 # pops latest without caching.
 # not gonna ether novel files, that seems pointless?
 def scrape():
-    (not os.path.isfile(".scm") or os.path.getsize(".scm") == 0) and exit()
+    (not os.path.isfile(SCM_NAME) or os.path.getsize(".scm") == 0) and exit()
     [
         open(k, "w").write("\n".join(v))
-        for k, v in json.loads(open(".scm", "r").read())["latest"].items()
+        for k, v in json.loads(open(SCM_NAME, "r").read())["latest"].items()
     ]
 
 
 # let's just roll back one, that's logically equivalent
 def revert():
     scrape()
-    scm = json.loads(open(".scm", "r").read())
+    scm = json.loads(open(SCM_NAME, "r").read())
     late = scm["latest"]
     curr = scm["commit"]
     len(curr) < 2 and exit()
-    last = l.pop()["diff"]
-    [os.system("echo " + "\n".join(v) + " | patch -R " + k) for k, v in last.items()]
+    last = curr.pop()["diff"]
+    for k, v in last.items():
+        open(DIFF_NAME, "w").write("\n".join(v))
+        os.system("patch -R " + k + " " + DIFF_NAME)
 
 
 viewer = lambda: os.system(
     "jq . .scm"
-)  # print(json.dumps(json.load(open(".scm")), indent=4))
+)  # print(json.dumps(json.load(open(SCM_NAME)), indent=4))
 
 __name__ == "__main__" and len(sys.argv) == 2 and {
     "commit": commit,
@@ -65,3 +70,6 @@ __name__ == "__main__" and len(sys.argv) == 2 and {
     "revert": revert,
     "viewer": viewer,
 }[sys.argv[1]]()
+
+# Trivial Comment
+
